37e17d62787b4438817c07c2790cf2d834575bcb
WenyuanShao <wenyuan@SEAS15672.seas.gwu.edu>
2019-09-22 19:29:23 -0400

    modify rate limit

--- test/mcblaster/multi_client.sh
+++ test/mcblaster/multi_client.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
 nodes=$1
+rate=$2
 core=0
 total_core=32
 fp=11211
 dp=11211
+idx=1
 
 for n in `seq $nodes`
 do
 	taskset -c $((core % total_core)) ./mcblaster \
 -t 1 -k 100 \
 -z 135 -u 11211 \
--f 11211 -r 100 \
+-f 11211 -r $rate \
 -d 30 10.10.1.2 > logs/mcb_$((fp)) &
 
 	fp=$((fp+1))
 	dp=$((dp+1))
 	core=$((core+1))
+	rate=$((rate*idx))
+	i=$((idx+1))
 done
 echo "started all client"
