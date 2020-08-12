91ef63147f009c8592b7cbfd8d2d9f3238c538d9
hcwang <hilarygwu@gmail.com>
2020-08-12 12:34:54 -0400

    multi client port

--- test/mcblaster/multi_client.sh
+++ test/mcblaster/multi_client.sh
@@ -5,13 +5,23 @@ nodes=$1
 rate=$2
 core=0
 total_core=1
+fp=10000
 dp=8888
 idx=1
 
-./mcblaster \
+for n in `seq $nodes`
+do
+	taskset -c $((core % total_core)) ./mcblaster \
 -t 1 -k 100 \
 -z 135 -u $((dp)) \
--r $((rate)) \
--d 30 127.0.0.1 
+-f $((fp)) -r $((rate)) \
+-d 30 127.0.0.1 &
+
+	fp=$((fp+1))
+	dp=$((dp))
+	core=$((core+1))
+	rate=$((rate*idx))
+	i=$((idx+1))
+done
 
 echo "started all client"
